3ac3699d3a8d38f2489830801a2c0f25c7eeae71
Blerim H <blerim.h@gmx.at>
2020-12-11 15:30:43 +0100

    fixed

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -24,10 +24,13 @@ export class HeroDetailComponent implements OnInit {
   }
 
   getHero(): void {
-    const id = +this.route.snapshot.paramMap.get('id');
-    this.heroService.getHero(id)
+    const id = this.route.snapshot.paramMap.get('id'); // 
+    if(id) {
+      this.heroService.getHero(+id)
       .subscribe(hero => this.hero = hero);
   }
+    }
+
 
   goBack(): void {
     this.location.back();
